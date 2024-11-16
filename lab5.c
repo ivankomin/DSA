01c45779a7279a994c49985c0d3828321ef6fa4b
Ivan Komin <ivankom0531@gmail.com>
2024-11-16 15:05:11 +0200

    fixed lab5

--- lab5.c
+++ lab5.c
@@ -10,11 +10,11 @@ int main(){
     }Stack;
 
     int N = 0, M = 0;
-    printf("Enter how many elements you want to insert: ");
-    scanf("%d", &N);
     Stack MyStack;
     MyStack.TopOfStack = 0;
-    double arr[15] = {9.3, 1.0, 7.8, 8.4, 3.9, 6.8, 9.0, 1.8, 5.4, 1.4, 3.9, 8.1, 1.9, 5.3, 1.6};
+    double arr[15] = {9.3, 1.0, 7.8, 8.4, 3.9, 6.8, 9.0, 1.8, 0.9, 1.4, 3.9, 8.1, 1.9, 5.3, 1.6};
+    printf("Enter how many elements you want to insert: ");
+    scanf("%d", &N);
     
     for (int i = 0; i < N; i++){
         if (MyStack.TopOfStack < SizeOfStack){
@@ -23,22 +23,21 @@ int main(){
         }
     }
     
-    double min = MyStack.elements[0];
-    int minIndex = 0;
+    double min = MyStack.elements[MyStack.TopOfStack - 1];
+    int minIndex = MyStack.TopOfStack - 1;
+    int originalTop = MyStack.TopOfStack;
     
-    for (int i = 1; i < MyStack.TopOfStack; i++){
-        if (MyStack.elements[i] < min) {
-            min = MyStack.elements[i];
-            minIndex = i;
+    while (MyStack.TopOfStack > 0) {
+        MyStack.TopOfStack--;
+        if (MyStack.elements[MyStack.TopOfStack] < min) {
+            min = MyStack.elements[MyStack.TopOfStack];
+            minIndex = MyStack.TopOfStack;
         }
     }
 
-    printf("Current size of the stack is %d.\n", MyStack.TopOfStack);
-    for (int i = 0; i < MyStack.TopOfStack; i++){
-        printf("%lf ", MyStack.elements[i]);
-    }
+    MyStack.TopOfStack = originalTop;
     
-    printf("\nEnter how many elements you want to delete: ");
+    printf("Enter how many elements you want to delete: ");
     scanf("%d", &M);
 
     while (M > 0 && MyStack.TopOfStack - 1 > minIndex) {
@@ -46,10 +45,5 @@ int main(){
         M--;
     }
 
-    printf("Current size of the stack is %d.\n", MyStack.TopOfStack);
-    for (int i = 0; i < MyStack.TopOfStack; i++){
-        printf("%lf ", MyStack.elements[i]);
-    }
     return 0;
 }
-
